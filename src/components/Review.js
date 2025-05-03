dafdc1515f54ccdcaf4488f8270bcd8639bd5d51
Prajwal <prajwal.ratnaparkhe18@gmail.com>
2025-05-03 00:05:51 -0500

    latest code update

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 
 // Import Swiper styles
 import "swiper/css";
